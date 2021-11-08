6d60c47b4c9bb4fe71d70db94867b73078acbd8f
ItiAgrawal5317 <bhumiaggarwal123@gmail.com>
2021-11-08 12:35:34 +0530

    Commit Multiply.java file

---
+++ SampleProject/src/Multiply.java
@@ -0,0 +1,12 @@
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1 = 25, n2 = 5, sum;  
+		sum = n1 * n2;  
+		System.out.println("The sum of numbers is: "+sum);   
+
+	}
+
+}
